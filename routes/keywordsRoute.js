84a7c55470cc0a80939880fd8e445574967e18a8
Fernando Spanhol <ferspanhol@gmail.com>
2020-01-17 22:06:15 -0300

    antes de multupload e bugfixes

--- routes/keywordsRoute.js
+++ routes/keywordsRoute.js
@@ -9,7 +9,7 @@ var Documento = require('../models/documentos');
 
 // Get lista de documentos
 router.get('/', ensureAuthenticated, function (req, res) {
-	console.log("get /documentos");
+	console.log("get /keyword");
 	var lista = [];
 	Documento.listar([], {}, function (err, result) {
 		var docs = result.rows;
@@ -27,31 +27,40 @@ router.get('/', ensureAuthenticated, function (req, res) {
 	});
 });
 
-// Get single document
-router.get('/:id', ensureAuthenticated, function (req, res) {
-	console.log("get /documentos/:" + req.body.id);
-	res.render('documento');
-});
-
-// Download a document
-router.get('/:id/download', ensureAuthenticated, function (req, res) {
-	console.log("get /documentos/:" + req.params.id + "/download");
-	var uploadDir = config.getUploadDir(__dirname);
-	Documento.getDocumentoById(req.params.id, function (err, documento) {
-		if (documento) {
-			console.log(documento);
-			if (uploadDir) {
-				var file = uploadDir + '/' + Documento.getFileName(documento);
-				console.log("uploadDir " + file);
-				res.download(file, documento.nome);
-			} else {
-				console.log("uploadDir FAILS");
+// Get single keyword
+router.get('/:k', ensureAuthenticated, function (req, res) {
+	console.log("get /keyword/" + req.params.k);
+	var key = [];
+	key.push(req.params.k);
+	var lista = [];
+	Documento.filterByKeywords(key, 1000, 0, function (err, result) {
+		
+		var docs = result.rows;
+		lista = [];
+		temKeyword = false;
+		console.log("CAllback " + key[0]  + "DOC len " + docs.length);
+		for (var i = 0; i < docs.length; i++) {
+			item = {}
+			item.id = docs[i].id;
+			item.nome = docs[i].nome;
+			item.data = docs[i].updated_at;
+			item.tipo = docs[i].tipo;
+			item.keywords = docs[i].keywords;
+			console.log("KEY " + key[0]);
+			for (var i = 0; i < item.keywords.length; i++) {
+				if (item.keywords[i] == key[0]){
+					temKeyword = true;
+					break;
+				}
+			}
+			if (temKeyword){
+				lista.push(item);
+				console.log("item " + item);
 			}
 		}
+		res.render('documentos', { documentos: lista, arquivo: true });
 	});
 });
-
-
 
 function ensureAuthenticated(req, res, next) {
 	if (req.isAuthenticated()) {
